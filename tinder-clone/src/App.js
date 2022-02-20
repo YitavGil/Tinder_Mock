7a0beb292a672d411fca4f356d61cc9e23c2311e
YitavGil <yitavga@yahoo.com>
2022-02-20 14:03:05 +0200

    first commit

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './comps/header/Header';
+import TinderCards from './comps/tindercards/TinderCards';
+
+
+function App() {
+  return (
+    <div className="app">
+    
+    <Header />
+    <TinderCards />
+    {/* SwipeButtons */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tinder-clone/src/comps/tindercards/TinderCards.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import './tinderCards.css';
+import TinderCard from 'react-tinder-card';
+
+const TinderCards = () => {
+    const [people, setPeople] = useState([
+        {
+            name: 'Mr. T',
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbxn7_p3a_THe0YKMY2gHZgBXM4k5nrhfYYiBWC6ZALJRG9lfGC8aZ5IVPGUXBKbMjUlo&usqp=CAU"
+        },
+        {
+            name: 'geralt of rivia',
+            url: "https://assets2.rockpapershotgun.com/the-witcher-3-best-rpgs.jpg/BROK/thumbnail/1200x1200/quality/100/the-witcher-3-best-rpgs.jpg"
+        },
+    ]);
+
+    const swiped = (direction, nameToDelete) => {
+      console.log("removing: " + nameToDelete);
+      setLastDirection(direction);
+    }
+
+
+  return (
+    <div className='tinder-cards'>
+        <div className="tinderCards-container">
+      {people.map((person) => (
+          <TinderCard
+          className='swipe'
+          key={person.name}
+          preventSwipe={['up', 'down']}
+          onSwipe={(dir) => swiped(dir. person.name)}
+          onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+
+          </TinderCard>
+      ))}
+        </div>
+    </div>
+  )
+}
+
+export default TinderCards
